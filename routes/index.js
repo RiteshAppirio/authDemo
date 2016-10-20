6a68edab898c76740e533026292782667cadfdcc
RiteshAppirio <ritesh@appirio.com>
2016-10-20 14:59:42 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express'});
 });
 
+router.get('/home',function(req,res){
+  res.send('<h1>Home</h1>');
+});
+router.get('/contact',function(req,rest) {
+  res.send('<h1>Contact Us</h1>');
+});
+
+
 module.exports = router;
